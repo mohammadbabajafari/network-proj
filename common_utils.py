1eb5d63b16ff33fb2150bc54828832fad8b48165
shirin <shirinmhb98@gmail.com>
2020-01-23 23:11:54 +0330

    add send_p2p send_flood function

--- common_utils.py
+++ common_utils.py
@@ -3,7 +3,7 @@ import sys
 import settings
 import json as JSON
 
-def send(data: dict , address: tuple):
+def base_send(data: dict , address: tuple):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverIpAddress = address[0]
@@ -12,5 +12,21 @@ def send(data: dict , address: tuple):
     sock.sendall(str.encode(JSON.dumps(data)))
     sock.close()
 
-def sendMsg(address, data: dict):
+def send_p2p(content: str, typ: str, address): #msg acc error qa   send to one specific   contetn type
+
+    data = {'type' : typ, 'content' : content}
+    base_send(data, address)
+
+def send_flood(content: str, typ: str, uuid: str): #q,b send to all  type contetn uuid
     
+    data = {'type' : typ, 'content' : content, 'uuid' : uuid}
+
+    #get followers or followings
+    if typ == 'Q':
+        f_list = []
+
+    elif typ == 'B':
+        f_list = []
+        
+    for ip in f_list:
+        base_send(data, ip)
