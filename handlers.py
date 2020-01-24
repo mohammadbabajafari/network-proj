79eb41c6bea5452e5ff2ff9e0549b981744cc11c
shirin <shirinmhb98@gmail.com>
2020-01-24 19:26:52 +0330

    add opendialog function

--- handlers.py
+++ handlers.py
@@ -1,22 +1,24 @@
 from common_utils import send_flood
 from data import Data
 from send_utils import send_accept
-
+from UI import newMessage, openDialog
 def handle_accept(json: dict, address: tuple, *args, **kwargs):
 
     Data.add_following(address[0])
-    alert_message = "user" , address[0] , "followed successfuly"
-    #show accept_alert(alert_message)
+    alert_message = f"user {address[0]} followed successfuly"
+    openDialog(alert_message)
+    
 
 def handle_error(json: dict, address: tuple, *args, **kwargs):
     content = json.get('content')
     alert_message = content.get('text')
-    #show accept_alert(alert_message)
+    openDialog(alert_message)
 
 
 def handle_broadcast(json: dict, address: tuple, *args, **kwargs):
     if not Data.has_flood_received(json.get('id')):
         Data.add_floods_received(json.get('id'))
+        newMessage(f'Broadcast: {address[0]}', json['content'].get('text'))
         send_flood(json.get('content'), json.get('type'), json.get('status'), json.get('id'))
 
 
