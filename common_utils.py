06cdf30c8c896be62b0832b89ce6909c79b4f405
MohammadBabajafari <mbabajafari@gmail.com>
2020-01-24 20:19:51 +0330

    fix uuid bug

--- common_utils.py
+++ common_utils.py
@@ -23,7 +23,7 @@ def send_p2p(content: dict, typ: str, status: int, address):  # msg acc error qa
 
 def send_flood(content: dict, typ: str, status: int, uuid: str):  # q,b send to all  type content uuid
 
-    data = {'type': typ, 'status': status, 'content': content, 'id': id}
+    data = {'type': typ, 'status': status, 'content': content, 'id': uuid}
 
     f_list = list()
 
