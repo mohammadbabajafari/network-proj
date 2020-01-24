c9b7dbfc195e6d5e267b7cd2fa5c0b53a9b9b993
MohammadBabajafari <mbabajafari@gmail.com>
2020-01-24 19:37:24 +0330

    fix import bug

--- handlers.py
+++ handlers.py
@@ -1,21 +1,24 @@
 from common_utils import send_flood
 from data import Data
 from send_utils import send_accept
-from UI import newMessage, openDialog
-def handle_accept(json: dict, address: tuple, *args, **kwargs):
 
+
+def handle_accept(json: dict, address: tuple, *args, **kwargs):
+    from UI import openDialog
     Data.add_following(address[0])
     alert_message = f"user {address[0]} followed successfuly"
     openDialog(alert_message)
-    
+
 
 def handle_error(json: dict, address: tuple, *args, **kwargs):
+    from UI import openDialog
     content = json.get('content')
     alert_message = content.get('text')
     openDialog(alert_message)
 
 
 def handle_broadcast(json: dict, address: tuple, *args, **kwargs):
+    from UI import newMessage
     if not Data.has_flood_received(json.get('id')):
         Data.add_floods_received(json.get('id'))
         newMessage(f'Broadcast: {address[0]}', json['content'].get('text'))
